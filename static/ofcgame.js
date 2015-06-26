193b241925f2a6d5f2d59e976077b077aa2eed90
Alastair <psyak3@nottingham.ac.uk>
2015-06-26 04:39:11 +0100

    updating for https://

--- static/ofcgame.js
+++ static/ofcgame.js
@@ -420,7 +420,7 @@ function POST_reqwest(thenFunction) {
     tarray2 = t[1];
 
     reqwest({
-            'url': 'http://alastairkerr.co.uk/ofc/api/ofc-backend/',
+            'url': 'https://alastairkerr.co.uk/ofc/api/ofc-backend/',
             'method': 'post',
             'data': {
                 "game-id": gameId,
